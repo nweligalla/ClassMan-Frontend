2a702c9f25a89f9b8a5b7dec41ef9307c69da5a9
nweligalla <nayanaweligalla@gmail.com>
2021-05-23 16:09:54 +0530

    added service methods

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'classman';
+
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {
+    this.employees = [];
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      }
+    ),
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+  }
+
+}
